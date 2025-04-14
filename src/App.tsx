eb5faf2e3167873c0290948437a2eeb61c5284ed
samaahmed1 <sama34777@gmail.com>
2025-04-14 14:48:26 +0200

    Add: Card Component , Update: JSON File & Update: Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Card from "./components/Card";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Card />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,42 @@
+import bagsData from "../data/data.json" assert { type: "json" };
+const bags = bagsData as Bag[];
+interface Bag {
+  id: string;
+  name: string;
+  year: number;
+  brand: string;
+  price: string;
+  image: string;
+}
+
+export default function Card() {
+  return (
+    <>
+      <section>
+        <div className="container mx-auto px-5 flex justify-center items-center flex-wrap gap-10">
+          {bags.map((bag) => {
+            return (
+              <div key={bag.id} className="card flex flex-col gap-3 shadow p-5 w-[350px] rounded-md">
+                <img className="rounded-md w-fit h-[250px] m-auto" src={bag.image} alt={bag.name} />
+                <h2 className="text-xl">
+                  {bag.name} {bag.year}
+                </h2>
+                <span className="text-gray-600">{bag.brand}</span>
+                <span className="font-medium">{parseInt(bag.price.replace("$", "")) > 60 ? `${bag.price} -` : bag.price}</span>
+                <span className="font-medium">
+                  {parseInt(bag.price.replace("$", "")) > 60 ? (
+                    <>
+                      <span className="text-xl text-red-500">Special Offer!</span>
+                    </>
+                  ) : (
+                    bag.price
+                  )}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+}
